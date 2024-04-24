1b65cde2db2ff4e47526417fe24eccbeedb5b6b7
KOROyo123 <1121910079@qq.com>
2024-04-24 08:43:39 +0800

    其他更新：修改了版本号和日志输出的版本格式

--- app/Koro_Caster_Service/src/main.cpp
+++ app/Koro_Caster_Service/src/main.cpp
@@ -128,7 +128,7 @@ int main(int argc, char **argv)
     // 初始化完成，开始进入正式流程
     spdlog::info("Start Log...");
     spdlog::info("Software: {}/{}", PROJECT_SET_NAME, PROJECT_SET_VERSION);
-    spdlog::info("Version : {}-{}", PROJECT_GIT_VERSION, PROJECT_TAG_VERSION);
+    spdlog::info("Version : {}", PROJECT_TAG_VERSION);
 
 #ifdef WIN32
     WSADATA wsaData;
