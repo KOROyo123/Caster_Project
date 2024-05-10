cb151fb5b024152db3af2a0bb716d778ad497980
KOROyo123 <1121910079@qq.com>
2024-05-10 15:19:56 +0800

    BUG修复：1、修复了源列表请求回复Header中Content-Length长度错误，该错误会导致bnc软件用ntrip2协议获取源列表失败； 2、同步修复了源列表请求回复Header中的Date日期错误，并为Ntrip1.0回复也增加了Date条目；

--- app/Koro_Caster_Service/src/Carrier/source_ntrip.cpp
+++ app/Koro_Caster_Service/src/Carrier/source_ntrip.cpp
@@ -96,10 +96,10 @@ int source_ntrip::build_source_table()
         evbuffer_add_printf(_evbuf, "Ntrip-Version: Ntrip/2.0\r\n");
         // evbuffer_add_printf(_evbuf, "Ntrip-Flag: st_filter,st_auth,st_match,st_strict,rtsp\r\n");
         evbuffer_add_printf(_evbuf, "Server: Koro_Caster/%s\r\n", PROJECT_TAG_VERSION);
-        evbuffer_add_printf(_evbuf, "Date: Tue, 01 Jan 2008 14:08:15 GMT\r\n");
+        evbuffer_add_printf(_evbuf, "Date: %s\r\n",util_get_http_date().c_str());
         evbuffer_add_printf(_evbuf, "Connection: close\r\n");
         evbuffer_add_printf(_evbuf, "Content-Type: gnss/sourcetable\r\n");
-        evbuffer_add_printf(_evbuf, "Content-Length: %ld\r\n", _source_list.size() + 17);
+        evbuffer_add_printf(_evbuf, "Content-Length: %ld\r\n", _source_list.size() + 16);
         evbuffer_add_printf(_evbuf, "\r\n");
         evbuffer_add(_evbuf, _source_list.c_str(), _source_list.size());
         evbuffer_add_printf(_evbuf, "ENDSOURCETABLE\r\n");
@@ -108,9 +108,10 @@ int source_ntrip::build_source_table()
     {
         evbuffer_add_printf(_evbuf, "SOURCETABLE 200 OK\r\n");
         evbuffer_add_printf(_evbuf, "Server: Koro_Caster/%s\r\n", PROJECT_TAG_VERSION);
+        evbuffer_add_printf(_evbuf, "Date: %s\r\n",util_get_http_date().c_str());
         evbuffer_add_printf(_evbuf, "Connection: close\r\n");
         evbuffer_add_printf(_evbuf, "Content-Type: text/plain\r\n");
-        evbuffer_add_printf(_evbuf, "Content-Length: %ld\r\n", _source_list.size() + 17);
+        evbuffer_add_printf(_evbuf, "Content-Length: %ld\r\n", _source_list.size() + 16);
         evbuffer_add_printf(_evbuf, "\r\n");
         evbuffer_add(_evbuf, _source_list.c_str(), _source_list.size());
         evbuffer_add_printf(_evbuf, "ENDSOURCETABLE\r\n");
