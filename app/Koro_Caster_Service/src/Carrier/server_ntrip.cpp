268bee0b343bcdf1439917f229673865208207d8
KOROyo123 <1121910079@qq.com>
2024-05-14 00:03:33 +0800

    其他更新：修改了心跳包的逻辑，现在会在连接成功的时候就发送一次心跳包

--- app/Koro_Caster_Service/src/Carrier/server_ntrip.cpp
+++ app/Koro_Caster_Service/src/Carrier/server_ntrip.cpp
@@ -95,6 +95,8 @@ int server_ntrip::runing()
         _timeout_tv.tv_usec = 0;
         _timeout_ev = event_new(bufferevent_get_base(_bev), -1, EV_PERSIST, TimeoutCallback, this);
         event_add(_timeout_ev, &_timeout_tv);
+
+        send_heart_beat_to_server();//直接就触发一次心跳包
     }
 
     return 0;
